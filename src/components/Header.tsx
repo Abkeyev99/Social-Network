4c017f0ac7c04581d3828f9eb24336953c3a69fc
Arhci <arlan.abkeyev@mail.ru>
2023-07-29 03:56:52 +0300

    import and export components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/YouTube_dark_icon_%282017%29.svg/2560px-YouTube_dark_icon_%282017%29.svg.png"/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            <div className="content">
+                <img src="https://secretmag.ru/imgs/2022/05/09/09/5397530/869f90de763ccf15b9cfe0d98beb2da083bd1b58.jpeg"/>
+            </div>
+            <div>Avatar</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>1 post</div>
+                <div>2 post</div>
+                <div>2 post</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
